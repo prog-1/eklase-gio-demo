b937511d1be1de715be58c94b2d8efd7d47b1266
Jaroslavs Samcuks <yarcat@gmail.com>
2022-05-01 16:29:43 +0200

    screen/list_students: alternate row color

--- screen/list_students.go
+++ screen/list_students.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 
 	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
@@ -13,14 +16,28 @@ import (
 func listStudents(th *material.Theme, vals *state.Handle) Screen {
 	var close widget.Clickable
 	list := widget.List{List: layout.List{Axis: layout.Vertical}}
+
+	lightContrast := th.ContrastBg
+	lightContrast.A = 0x11
+	darkContrast := th.ContrastBg
+	darkContrast.A = 0x33
 	return func(gtx layout.Context) (Screen, layout.Dimensions) {
 		d := layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-			layout.Flexed(1, rowInset(func(gtx layout.Context) layout.Dimensions {
+			layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
 				return material.List(th, &list).Layout(gtx, vals.StudentCount(), func(gtx layout.Context, index int) layout.Dimensions {
 					student := vals.Student(index)
-					return material.Body1(th, fmt.Sprintf("%s %s", student.Surname, student.Name)).Layout(gtx)
+					rec := op.Record(gtx.Ops)
+					d := rowInset(material.Body1(th, fmt.Sprintf("%s %s", student.Surname, student.Name)).Layout)(gtx)
+					macro := rec.Stop()
+					color := lightContrast
+					if index%2 == 0 {
+						color = darkContrast
+					}
+					paint.FillShape(gtx.Ops, color, clip.Rect{Max: d.Size}.Op())
+					macro.Add(gtx.Ops)
+					return d
 				})
-			})),
+			}),
 			layout.Rigid(rowInset(material.Button(th, &close, "Close").Layout)),
 		)
 		if close.Clicked() {
